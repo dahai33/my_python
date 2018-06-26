d950ad92b1a70169848de1c10bcab9dd29d8a0ed
liuhaitao <liuhaitaodahai@sina.com>
2018-06-26 16:26:15 +0800

    this is the test01

--- src/Lesson_100/Lesson_01.py
+++ src/Lesson_100/Lesson_01.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+#Author:刘海涛
+# --*-- coding: uft-8 --*--
+# @Time   : 16:24
 print ("git hub test")
-
 print ("you can you try")
---
+++ src/Lesson_100/Lesson_02.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#Author:刘海涛
+# --*-- coding: uft-8 --*--
+# @Time   : 16:26
+
+#定义变量和常量
+var_1=1
+x=2
+print(id(x))
