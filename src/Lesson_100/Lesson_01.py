d22006d1d3db4115bae0b54b54894ff3de986067
liuhaitao <liuhaitaodahai@sina.com>
2018-06-27 19:33:42 +0800

    this is the test02

--- src/Lesson_100/Lesson_01.py
+++ src/Lesson_100/Lesson_01.py
@@ -2,7 +2,9 @@
 #Author:刘海涛
 # --*-- coding: uft-8 --*--
 # @Time   : 16:24
-print ("git hub test")
-print ("you can you try")
-print ("is the test")
-
+# 局部变量#
+local=1
+def fun():
+    local=1
+    print("局部变量local")
+fun()
