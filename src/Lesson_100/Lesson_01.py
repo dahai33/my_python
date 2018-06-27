694eaeeb0b7cff6f214e50af1181e7fd83eadf69
liuhaitao <liuhaitaodahai@sina.com>
2018-06-27 20:49:12 +0800

    this is the test03

--- src/Lesson_100/Lesson_01.py
+++ src/Lesson_100/Lesson_01.py
@@ -8,3 +8,33 @@ def fun():
     local=1
     print("局部变量local")
 fun()
+#global 用于全局变量的引用 类型定义使用
+int=10
+str="hello word"
+#输出字符串
+str1="hello:\"liuhaitao\"!"
+str2='''liu"haitao"'''
+#换行
+str3="liuhaitaodhaia@sina.com \n password"
+#输出特殊字符
+str4="##### ***** () $$ %%%"
+i=2**38
+print(i)
+print("###$$$")
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+##第一章课后题
+# def sum(i ,sum ):
+#     if(i<0)：
+#         print("请输入大于0的参数")
+#     elif(i==0)：
+#         print("您输入的参数为0")
+#     else：
+#         for a < i:
+#             sum=sum+i
+#             print(sum)
+
+
+
