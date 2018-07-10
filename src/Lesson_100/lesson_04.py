5baf49f901d112fc77a1b336ef1662bccfca6ca1
liuhaitao <liuhaitaodahai@sina.com>
2018-07-11 00:36:11 +0800

    this is test0711

--- src/Lesson_100/lesson_04.py
+++ src/Lesson_100/lesson_04.py
@@ -13,3 +13,11 @@ import datetime as time
 #     print('{0:3d} {1:5d} {2:14d}'.format(x,x*x,x*x*x))
 #table={'abc':1,'bcd':2,'def':3}
 #print('abc:{abc:d}; bcd:{bcd:d};def:{def:d}'.format(table))
+
+import time
+localtime=time.localtime(time.time())
+loca=time.asctime(time.localtime(time.time()))
+print(loca)
+print(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()))
+a = "Sat Mar 28 22:24:24 2016"
+print (time.mktime(time.strftime(a,"%a %b %d %H:%M:%S %Y")))
--- src/Lesson_100/lesson_05.py
+++ src/Lesson_100/lesson_05.py
@@ -2,7 +2,6 @@
 #Author:刘海涛
 # --*-- coding: uft-8 --*--
 # @Time   : 16:26
-#f=open('/Users/apple/Documents/test.txt','r+')
 # str=f.read()
 # f.close()
 # print(str)
@@ -17,5 +16,20 @@
 # f.write(s)
 # f.seek(3,2)
 # f.close()
-import pickle as pi
-x=pi.load("/Users/apple/Documents/test.txt")
+# import pickle as pi
+# x=pi.load("/Users/apple/Documents/test.txt")
+# try:
+#     print("is the")
+# except:
+#     print("is bad")
+#     print(Exception)
+# finally:
+#     pass
+
+# try:
+#     f=open('/Users/apple/Documents/test.txt','r+')
+#     f.write("is the good")
+# except IOError:
+#     print("is IO error")
+# finally:
+#     f.close()
