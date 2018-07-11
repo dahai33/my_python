8799909b0a6ba58ed873c85fa3e3229d802b6f4f
liuhaitao <liuhaitaodahai@sina.com>
2018-07-11 19:45:14 +0800

    0711 测试

---
+++ src/Lesson_100/lesson_08.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#Author:刘海涛
+# --*-- coding: uft-8 --*--
+# @Time   : 19:32
+#@字符串正则表达
+str1="version"
+num=1.0
+sum =100
+format="%s" % str1
+format="%s,%.4f,%%" %(str1,num,sum)
+print(format)
