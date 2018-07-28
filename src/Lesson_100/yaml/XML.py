bcbe88a1de0686f70bd4a5111e61edea7b0130f6
liuhaitao <liuhaitaodahai@sina.com>
2018-07-28 17:50:19 +0800

    0719

--- src/Lesson_100/yaml/XML.py
+++ src/Lesson_100/yaml/XML.py
@@ -6,3 +6,4 @@ import xml.dom.minidom
 
 dom=xml.dom.minidom.parse("test.xml")
 
+# http://mio4kon.com/2017/04/13/%E7%A7%BB%E5%8A%A8%E7%AB%AF%E8%87%AA%E5%8A%A8%E5%8C%96%E6%B5%8B%E8%AF%95%E7%B3%BB%E5%88%97%E4%B9%8B%E5%9B%9B%E2%80%94%E2%80%94%E7%94%9F%E6%88%90%E5%AE%9A%E4%BD%8D%E5%85%83%E7%B4%A0/
